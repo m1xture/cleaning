983e05b1e8cf58b11debc4a0c976b424406583d3
m1xture <misha.ivanov.1050@gmail.com>
2024-05-09 17:39:10 +0300

    validation fix

--- src/js/orderValidation.js
+++ src/js/orderValidation.js
@@ -1,3 +1,5 @@
+import showError from './errorShow';
+
 export default function validateOrder({
   username,
   type,
@@ -9,21 +11,27 @@ export default function validateOrder({
   let errors = 0;
   if (username.length <= 3) {
     errors++;
+    showError('username');
   }
   if (!type) {
     errors++;
+    showError('type');
   }
   if (isNaN(rooms) || rooms <= 0) {
     errors++;
+    showError('rooms');
   }
   if (phone.length <= 6) {
     errors++;
+    showError('phone');
   }
   if (!district) {
     errors++;
+    showError('district');
   }
   if (address.length <= 5) {
     errors++;
+    showError('address');
   }
   if (errors === 0) {
     return true;
