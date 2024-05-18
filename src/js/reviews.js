13cb9e26f3d8c472648d06195c38c40b8768e218
Oleksandr1shkin <cmurtimur@gmail.com>
2024-05-18 11:59:44 +0300

    зробив делегування

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,48 +1,21 @@
-const stars = document.querySelectorAll('.reviews__star');
-const stars2 = document.querySelectorAll('.reviews__star-2');
-const stars3 = document.querySelectorAll('.reviews__star-3');
-
-stars.forEach((star, index) => {
-    star.addEventListener('mouseover', () => {
-        for (let i = 0; i <= index; i++) {
-            stars[i].querySelector('svg path').style.fill = 'yellow';
-
+document.querySelectorAll('.reviews__star-list').forEach((list) => {
+    list.addEventListener('mouseover', (event) => {
+        if (event.target.closest('li')) {
+            const star = event.target.closest('li');
+            const index = Array.from(list.children).indexOf(star);
+            for (let i = 0; i <= index; i++) {
+                list.children[i].querySelector('svg path').style.fill = 'yellow';
+            }
         }
     });
 
-    star.addEventListener('mouseout', () => {
-        for (let i = 0; i <= index; i++) {
-            stars[i].querySelector('svg path').style.fill = '#D9D9D9';
-        }
-    });
-});
-
-stars2.forEach((star, index) => {
-    star.addEventListener('mouseover', () => {
-        for (let i = 0; i <= index; i++) {
-            stars2[i].querySelector('svg path').style.fill = 'yellow';
-
-        }
-    });
-
-    star.addEventListener('mouseout', () => {
-        for (let i = 0; i <= index; i++) {
-            stars2[i].querySelector('svg path').style.fill = '#D9D9D9';
-        }
-    });
-});
-
-stars3.forEach((star, index) => {
-    star.addEventListener('mouseover', () => {
-        for (let i = 0; i <= index; i++) {
-            stars3[i].querySelector('svg path').style.fill = 'yellow';
-
-        }
-    });
-
-    star.addEventListener('mouseout', () => {
-        for (let i = 0; i <= index; i++) {
-            stars3[i].querySelector('svg path').style.fill = '#D9D9D9';
+    list.addEventListener('mouseout', (event) => {
+        if (event.target.closest('li')) {
+            const star = event.target.closest('li');
+            const index = Array.from(list.children).indexOf(star);
+            for (let i = 0; i <= index; i++) {
+                list.children[i].querySelector('svg path').style.fill = '#D9D9D9';
+            }
         }
     });
 });
